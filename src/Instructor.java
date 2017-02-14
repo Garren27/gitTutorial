4c9c2d7a79a8e73f872432e9f415f2d154d3d9bf
Garren27 <gmccall5@uwo.ca>
2017-02-13 22:53:21 -0500

    Added Course class and made changes to Student and Instructor

--- src/Instructor.java
+++ src/Instructor.java
@@ -3,10 +3,12 @@
  */
 public class Instructor extends Person {
     String title;
+    Course favcourse;
 
-    public Instructor(String name, String emailAddress, String title){
+    public Instructor(String name, String emailAddress, String title, Course favcourse){
         super(name,emailAddress);
         this.title=title;
+        this.favcourse = favcourse;
     }
 
     public String toString(){
